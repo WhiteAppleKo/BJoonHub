84ee9d3173b0307819a7515dd9b11421f1c909c9
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2023-05-05 15:41:44 +0900

    [Bronze V] Title: 합, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int Num = 0;
+		for(int i = 1; i <= A; i++) {
+		Num += i;
+			
+		}
+		System.out.println(Num);
+	}
+
+}

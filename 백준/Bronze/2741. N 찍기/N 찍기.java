b8cd5568fd6ecd7e2313667667dec637293c90a9
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2023-05-06 15:59:03 +0900

    [Bronze V] Title: N 찍기, Time: 988 ms, Memory: 29424 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		
+		int Num = Integer.parseInt(st.nextToken());
+		for(int i = 1 ; i < Num + 1 ; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}

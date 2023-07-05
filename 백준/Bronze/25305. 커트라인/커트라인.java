3597453b381019d7f4fea8f37e3d0aa1b84d8ee1
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2023-07-05 18:56:57 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		Integer TN [] = new Integer [N];
+		for(int i = 0; i < N; i++) {
+			TN[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(TN, Comparator.reverseOrder());
+		
+		System.out.println(TN[K - 1]);
+		
+	}
+}

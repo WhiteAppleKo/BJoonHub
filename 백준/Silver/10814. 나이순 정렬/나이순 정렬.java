b62953ddb9d2c1f354ebeaf3fa1622bc620f7faa
WhiteAppleKo <102124768+WhiteAppleKo@users.noreply.github.com>
2023-07-05 18:58:34 +0900

    [Silver V] Title: 나이순 정렬, Time: 1644 ms, Memory: 61636 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		String arr[][] = new String[N][2];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			arr[i][0] = st.nextToken();
+			arr[i][1] = st.nextToken();
+		}
+		Arrays.sort(arr, new Comparator<String[]>() {
+			public int compare(String s1[], String s2[]) {
+				return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+			}
+		});
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+	}
+}
